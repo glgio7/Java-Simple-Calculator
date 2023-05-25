d2305c377c329ba8fa091f07e71a216086011b0c
GLGIO7 <giovanelucas00@outlook.com>
2023-05-25 17:01:03 -0300

    refactor: using switch case to get result

--- src/App.java
+++ src/App.java
@@ -3,20 +3,37 @@ import javax.swing.JOptionPane;
 class Calculator {
     public static void main (String entrada[]) {
         // Variables declaration
-        int num1, num2;
+        float num1, num2, result;
         char operator;
-        float result;
         
         // Get numbers and operator from user input using JOptionPane lib
-        num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o primeiro número inteiro:"));
+        num1 = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite o primeiro número:"));
         operator = JOptionPane.showInputDialog(null, "Digite o operador matemático:").charAt(0);
-        num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o segundo número inteiro:"));
+        num2 = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite o segundo número:"));
         
 
         // Get result from math operation
-        result = operator == "/".charAt(0) ? (float)num1 / num2 : operator == "*".charAt(0) ? (float)num1 * num2 : operator == "-".charAt(0) ? (float)num1 - num2 : (float)num1 + num2;
-        
-        // Show result
-        JOptionPane.showMessageDialog(null, "O resultado da operaçãp é: " + result);
+        switch (operator){
+            // Single quotes '' are used to represent char, while double quotes "" are used to represent strings
+            case '/':
+                result = num1 / num2;
+                JOptionPane.showMessageDialog(null, "O resultado da operação é: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                JOptionPane.showMessageDialog(null, "O resultado da operação é: " + result);
+                break;
+            case '+':
+                result = num1 + num2;
+                JOptionPane.showMessageDialog(null, "O resultado da operação é: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                JOptionPane.showMessageDialog(null, "O resultado da operação é: " + result);
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Operador inválido!");
+                break;
+        }
     }
 }
